4b02cdfbe22e4c1a87552ba6bbd9222f8761de0d
hyowo <vilumaaerik@gmail.com>
2023-06-05 16:04:12 +0300

    Changed Session model

--- app-myconference/Models/Session.cs
+++ app-myconference/Models/Session.cs
@@ -1,5 +1,6 @@
-﻿namespace  app_myconference.Models;
-internal class Session
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+namespace  app_myconference.Models;
+public partial class Session : ObservableObject
 {
     public int Id { get; set; }
     public string Title { get; set; }
@@ -9,4 +10,14 @@ internal class Session
     public DateTime End { get; set; }
     public string StartTimeDisplay => $"{Start:t}";
     public string DayDisplay => $"{Start:MMM} {Start:d}";
+    public Speaker Speaker { get; set; } = new Speaker
+    {
+        Name = "Tester Testing",
+        Company = "Microsoft",
+        Description = "Oh No This is a Description",
+        Title = "Principal Lead Program Manager"
+    };
+
+    [ObservableProperty]
+    bool inAgenda;
 }
