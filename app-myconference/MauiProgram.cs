68eae925737c4b41aaa3c3ffbfb1be3a323eeed5
hyowo <vilumaaerik@gmail.com>
2023-06-05 16:53:10 +0300

    Edited AgendaPage

--- app-myconference/MauiProgram.cs
+++ app-myconference/MauiProgram.cs
@@ -1,4 +1,7 @@
-﻿namespace app_myconference;
+﻿using app_myconference.Pages;
+using app_myconference.ViewModels;
+
+namespace app_myconference;
 
 public static class MauiProgram
 {
@@ -13,7 +16,8 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		builder.Services.AddSingleton<>
+		builder.Services.AddTransient<SchedulePage>();
+		builder.Services.AddTransient<ScheduleViewModel>();
 
 		return builder.Build();
 	}
