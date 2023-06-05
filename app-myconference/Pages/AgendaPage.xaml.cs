32fbfd548f8b32d7b49089d2b8efb591e07d5bee
hyowo <vilumaaerik@gmail.com>
2023-06-05 17:32:15 +0300

    fixes to schedulePage

--- app-myconference/Pages/AgendaPage.xaml.cs
+++ app-myconference/Pages/AgendaPage.xaml.cs
@@ -4,32 +4,8 @@ namespace app_myconference.Pages;
 
 public partial class AgendaPage : ContentPage
 {
-    readonly ScheduleViewModel vm;
-
-    public AgendaPage(ScheduleViewModel vm)
-	{
-		InitializeComponent();
-		
-		switch(Title)
-		{
-			case "Day 1":
-				vm.Day = 1;
-				break;
-            case "Day 2":
-                vm.Day = 2;
-                break;
-        }
-        BindingContext = this.vm = vm;
-    }
-
-
-
-    protected override async void OnNavigatedTo(NavigatedToEventArgs args)
+    public AgendaPage()
     {
-        base.OnNavigatedTo(args);
-        if(vm.Schedule.Count == 0)
-        {
-            await vm.LoadDataCommand.ExecuteAsync(null);
-        }
+        InitializeComponent();
     }
 }
